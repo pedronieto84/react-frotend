c596d8053c7226c5d18176cb0228ae37334fcfeb
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-20 10:34:11 +0200

    Ejemplo de ternario, objects, y funciones en JSX.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ const nombre = ()=>{
   return 'Luis'
 }
 
+const usuario = {
+  direccion: {
+    calle: 'Avenida LlucMajor'
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <h1> HOla { nombre() }</h1>
+      <h1>2+2</h1>
+      <h1> Matematics { 2+2 }</h1>
+      <h1> Ejemplo de Ternario { (3+2 ) > 4 ? 'es mayor': 'es menor'}</h1>
+      <h1> Ejemplo de un Object {usuario.direccion.calle }</h1>
     </div>
   );
 }
