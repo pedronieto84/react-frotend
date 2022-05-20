9cc4ecd4724e601ad7f57c3e74ef7e730bc3b4c2
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-05-20 10:30:52 +0200

    Como printear en pantalla los datos cargandolos de una variable.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 
+const nombre = 'Antonio'
+
 function App() {
   return (
     <div className="App">
-  <h1> HOla Mundo</h1>
+      <h1> HOla { nombre }</h1>
     </div>
   );
 }
